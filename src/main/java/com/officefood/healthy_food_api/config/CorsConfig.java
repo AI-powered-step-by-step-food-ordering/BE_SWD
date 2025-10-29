b46d5b0c07e3005c38e4454c26dc475111158448
Tran Hoang Vu <hoangvutran088@gmail.com>
2025-10-29 11:45:22 +0700

    Create CorsConfig.java

---
+++ src/main/java/com/officefood/healthy_food_api/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.officefood.healthy_food_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // Cho phép tất cả origin
+        config.addAllowedHeader("*"); // Cho phép tất cả header
+        config.addAllowedMethod("*"); // Cho phép tất cả method
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
